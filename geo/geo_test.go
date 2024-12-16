bc7f699a745597c71e2465f69d5d2f6c441c9853
Aleksey Sokolov <alexis871aa@yandex.ru>
2024-12-16 22:11:46 +0300

    Начали изучения раздела тестирования

---
+++ geo/geo_test.go
@@ -0,0 +1,11 @@
+package geo_test
+
+import "testing"
+
+func TestGetMyLocation(t *testing.T) {
+	// Arange - подготовка теста, в ней мы определяем expected результат, данные для функции тестирования
+
+	// Act - выполняем функцию, с заданными в моменте Arange данными
+
+	// Assert - проверка результата работы с expected
+}
